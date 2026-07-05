7b26ff65c506589b70fda3f44def88e15e2f38ce
proskahn <k13659082460@gmail.com>
2026-07-05 13:56:05 +0000

    got one solution

---
+++ opt/run_optimization.py
@@ -0,0 +1,5 @@
+ # It runs a black-box multi-objective optimization of the elctrolyzer.
+#  At this moment, the decision variable is the membrane thickness, the objectives are 
+# 1)cell voltage and 2) gas cross over.
+# In this case, the optimization loop is 
+# Give a membrane thickness, then run the simulation and check the objectives. Run 50 iterations.
