0a40a3a6d328ae4d959df243cb6fb3797aef1f3d
proskahn <k13659082460@gmail.com>
2026-07-11 08:41:50 +0000

    nothing happend

--- opt/run_optimization.py
+++ opt/run_optimization.py
@@ -3,3 +3,4 @@
 # 1)cell voltage and 2) gas cross over.
 # In this case, the optimization loop is 
 # Give a membrane thickness, then run the simulation and check the objectives. Run 50 iterations.
+# Plot the pareto front at the end of the optimization.
