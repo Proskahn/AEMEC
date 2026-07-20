bbf622552a8846de2d39781936a081f3d687547e
proskahn <k13659082460@gmail.com>
2026-07-20 18:02:09 +0000

    new visualization

--- visualization/I(t)_curve.py
+++ visualization/I(t)_curve.py
@@ -1,14 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-t = np.linspace(0, 90, 501)  # Time, s
-I = 1.3 + t/75     # Current density, A/cm²
+t = np.linspace(0, 160, 501)  # Time, s
+I = 1.3 + t/160     # Current density, A/cm²
 
 plt.plot(t, I, linewidth=2)
 plt.xlabel("Time (s)")
 plt.ylabel(r"Cell Voltage (V)")
-plt.title(r"$I(t)=1.3 + \frac{t}{75}\ \mathrm{V}$")
-plt.xlim(0, 90)
+plt.title(r"$I(t)=1.3 + \frac{t}{160}\ \mathrm{V}$")
+plt.xlim(0, 160)
 plt.ylim(1.3, 2.5)
 plt.grid(True, alpha=0.3)
 plt.tight_layout()
